5b27c98d88fc844e502a7a7613969fb931e080f7
Yanzhao Wang <wyz159753@gmail.com>
2017-03-23 18:21:14 +0800

    fix video fields

--- backend/models/video.py
+++ backend/models/video.py
@@ -11,7 +11,7 @@ def video_formatter(t):
     return {
         'video_id': t[0],
         'video_name': t[1],
-        'video_address': t[2],
+        'video_address': os.path.join('data', t[2]),
         'screenshot': t[3]
     }
 

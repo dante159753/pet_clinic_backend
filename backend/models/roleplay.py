43cc28b039d2473fbdadc5aa3fe35b910767d0a7
Yanzhao Wang <wyz159753@gmail.com>
2017-03-28 12:03:02 +0800

    add roleplay

---
+++ backend/models/roleplay.py
@@ -0,0 +1,303 @@
+# -*- coding: utf-8 -*-
+
+from backend import mysql
+from backend.util import format_by_formater
+from .video import VideoHelper
+from .picture import PictureHelper
+from .department import DepartHelper
+
+def roleplay_info_formatter(t):
+    return {
+        'role_id': t[0],
+        'role_name': t[1],
+        'role_desc': t[2],
+        'page_size': t[3],
+        'depart_id': t[4],
+        'picture': t[5]
+    }
+
+def roleplay_page_formatter(t):
+    return {
+        'role_info': RoleplayInfoHelper.get_by_id(t[0]),
+        'page_info': RoleplayPageInfoHelper.get_by_pagination(t[0], t[1]),
+        'page_content': RoleplayContentHelper.get_by_page_id(t[2])
+    }
+
+def roleplay_page_info_formatter(t):
+    return {
+        'page_id': t[0],
+        'page_title': t[1],
+        'pagination': t[2],
+    }
+
+def roleplay_content_formatter(t):
+    return {
+        'content_id': t[0],
+        'content_type': t[1],
+        'text_content': t[2],
+        'picture_content': PictureHelper.get_by_id(t[3]) if t[3] else None,
+        'video_content': VideoHelper.get_by_id(t[4]) if t[4] else None
+    }
+
+class RoleplayInfoHelper:
+    @staticmethod
+    @format_by_formater(roleplay_info_formatter)
+    def get_by_id(role_id):
+        cursor = mysql.get_db().cursor()
+        cursor.execute(
+            'select id, name, description, page_size, depart_id, picture from role where id=%s', 
+            [role_id]
+            )
+        return cursor.fetchone()
+
+    @staticmethod
+    @format_by_formater(roleplay_info_formatter)
+    def get_by_name(role_name):
+        cursor = mysql.get_db().cursor()
+        cursor.execute(
+            'select id, name, description, page_size, depart_id, picture from role where name=%s', 
+            [role_name]
+            )
+        return cursor.fetchone()
+
+    @staticmethod
+    @format_by_formater(roleplay_info_formatter, True)
+    def get_all():
+        cursor = mysql.get_db().cursor()
+        cursor.execute('select id, name, description, page_size, depart_id, picture from role')
+        return cursor.fetchall()
+
+    @staticmethod
+    def create_roleplay_info(role_name, role_desc, depart_id, picture_id):
+        if RoleplayInfoHelper.get_by_name(role_name) is not None:
+            return False, 'duplicate role play name'
+
+        if not PictureHelper.get_by_id(picture_id):
+            return False, 'invalid picture_id'
+
+        if not DepartHelper.get_by_id(depart_id):
+            return False, 'invalid depart_id'
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "insert into role (name, description, page_size, depart_id, picture) "
+            "values (%s, %s, '0', %s, %s)", 
+            (role_name, role_desc, depart_id, picture_id)
+            )
+        db.commit()
+        # row count为1表示插入成功
+        if cursor.rowcount != 1:
+            return False, 'insert into db failed'
+        else:
+            return True, cursor.lastrowid
+
+    @staticmethod
+    def modify(role_id, fields):
+        if RoleplayInfoHelper.get_by_id(role_id) is None:
+            return False, 'can not find role_id'
+
+        set_sql = reduce(
+            (lambda s1, s2: s1 +',' + s2), 
+            ["{}=%s".format(k) for (k, v) in fields]
+            )
+        print set_sql
+
+        arg_list = [v for (k, v) in fields]
+        arg_list.append(role_id)
+        print arg_list
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "update role set {} where id=%s".format(set_sql),
+            arg_list
+            )
+        db.commit()
+        return cursor.rowcount == 1, None
+
+    @staticmethod
+    def delete_by_id(role_id):
+        # TODO: delete all sub infos
+        if RoleplayInfoHelper.get_by_id(role_id) is None:
+            return False, 'can not find role_id'
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "delete from item_info where id=%s", 
+            (role_id,)
+            )
+        db.commit()
+        return cursor.rowcount == 1, None
+
+
+class RoleplayPageInfoHelper:
+    @staticmethod
+    @format_by_formater(roleplay_page_info_formatter)
+    def get_by_pagination(role_id, pagination):
+        cursor = mysql.get_db().cursor()
+        cursor.execute(
+            'select id, title, pagination from role_page where role_id=%s and pagination=%s', 
+            [role_id, pagination]
+            )
+        return cursor.fetchone()
+
+    @staticmethod
+    @format_by_formater(roleplay_page_info_formatter)
+    def get_by_id(page_id):
+        cursor = mysql.get_db().cursor()
+        cursor.execute(
+            'select id, title, pagination from role_page where id=%s', 
+            [page_id]
+            )
+        return cursor.fetchone()
+
+    @staticmethod
+    @format_by_formater(roleplay_page_info_formatter, True)
+    def get_by_roleid(role_id):
+        cursor = mysql.get_db().cursor()
+        cursor.execute(
+            'select id, title, pagination from role_page where role_id=%s', 
+            [role_id]
+            )
+        return cursor.fetchall()
+
+    @staticmethod
+    def create_page_info(role_id, page_title, contents):
+        if not RoleplayInfoHelper.get_by_id(role_id):
+            return False, 'invalid role_id'
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+
+        # get page size
+        cursor.execute(
+            'select count(*) from role_page where role_id=%s', 
+            (role_id,)
+            )
+        page_size = curosr.fetchone()[0]
+        print page_size
+        # insert role_page
+        cursor.execute(
+            "insert into role_page (role_id, title, pagination) "
+            "values (%s, %s, %s)", 
+            (role_id, page_title, int(page_size) + 1)
+            )
+        db.commit()
+        page_id = cursor.lastrowid
+        for content in contents:
+            RoleplayContentHelper.create_content(page_id, content)
+
+        return True, page_id
+
+    @staticmethod
+    def modify(role_id, pagination, page_title, contents):
+        if RoleplayPageInfoHelper.get_by_pagination(role_id, pagination) is None:
+            return False, 'can not find page'
+
+        page = RoleplayPageInfoHelper.get_by_pagination(role_id, pagination)
+        RoleplayContentHelper.delete_by_page_id(page['page_id'])
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "update role_page set title=%s where id=%s",
+            (page_title, page['page_id'])
+            )
+        db.commit()
+        return cursor.rowcount == 1, None
+
+    @staticmethod
+    def delete(role_id, pagination):
+        # TODO: delete all sub infos
+        if RoleplayInfoHelper.get_by_pagination(role_id, pagination) is None:
+            return False, 'can not find page'
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "delete from item_info where role_id=%s and pagination=%s", 
+            (role_id, pagination,)
+            )
+        db.commit()
+        return cursor.rowcount == 1, None
+
+
+class RoleplayPageHelper:
+    @staticmethod
+    @format_by_formater(roleplay_page_formatter)
+    def get_by_pagination(role_id, pagination):
+        return [role_id, pagination, RoleplayPageInfoHelper.get_by_pagination(role_id, pagination)['page_id']]
+
+
+    @staticmethod
+    @format_by_formater(roleplay_page_formatter)
+    def get_by_id(role_id, page_id):
+        return [role_id, RoleplayPageInfoHelper.get_by_id(page_id)['pagination'], page_id]
+
+
+class RoleplayContentHelper:
+    @staticmethod
+    @format_by_formater(roleplay_content_formatter, True)
+    def get_by_page_id(page_id):
+        cursor = mysql.get_db().cursor()
+        cursor.execute(
+            'select id, content_type, text_content, picture_id, video_id from role_page_content where page_id=%s', 
+            [page_id]
+            )
+        return cursor.fetchall()
+
+    @staticmethod
+    def create_content(page_id, content):
+        content_type = content['content_type']
+
+        sql_content = None # 字段名
+        arg_content = None # 内容
+        if str(content_type) == '1':
+            sql_content = 'text_content'
+            arg_content = content['text_content']
+        elif str(content_type) == '2':
+            sql_content = 'video_id'
+            arg_content = content['video_content']
+        elif str(content_type) == '3':
+            sql_content = 'picture_id'
+            arg_content = content['picture_content']
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "insert into role_page_content (page_id, content_type, {}) values (%s, %s, %s)".format(sql_content),
+            (page_id, content_type, arg_content)
+            )
+        db.commit()
+        # row count为1表示插入成功
+        if cursor.rowcount != 1:
+            return False
+        return cursor.lastrowid
+
+
+    @staticmethod
+    def delete_by_id(content_id):
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "delete from role_page_content where id=%s", 
+            (content_id,)
+            )
+        db.commit()
+        return cursor.rowcount == 1
+
+    @staticmethod
+    def delete_by_page_id(page_id):
+            
+        db = mysql.get_db()
+        cursor = db.cursor()
+        cursor.execute(
+            "delete from role_page_content where page_id=%s", 
+            (page_id,)
+            )
+        db.commit()
+        return True
+
---
+++ backend/resources/roleplay.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+from flask_restful import Resource, fields, marshal_with, abort, reqparse
+from backend.models.roleplay import RoleplayInfoHelper, RoleplayPageInfoHelper, RoleplayPageHelper, RoleplayContentHelper
+from .picture import picture_fields
+from .video import video_fields
+import json
+
+roleplay_info_fields = {
+    'role_id': fields.Integer,
+    'role_name': fields.String,
+    'role_desc': fields.String,
+    'page_size': fields.Integer,
+    'depart_id': fields.Integer,
+    'picture': fields.Integer,
+}
+
+page_content = {
+    'content_id': fields.Integer,
+    'content_type': fields.Integer,
+    'text_content': fields.String,
+    'picture_content': fields.Nested(picture_fields),
+    'video_content': fields.Nested(video_fields)
+}
+
+page_info_fields = {
+    'page_id': fields.Integer,
+    'pagination': fields.Integer,
+    'page_title': fields.String
+}
+
+roleplay_page_fields = {
+    'role_info': fields.Nested(roleplay_info_fields),
+    'page_info': fields.Nested(page_info_fields),
+    'page_content': fields.List(fields.Nested(page_content))
+}
+
+class RoleplayInfo(Resource):
+    @marshal_with(roleplay_info_fields)
+    def get(self):
+        result = RoleplayInfoHelper.get_all()
+        if result is None:
+            print 'no role play info'
+            abort(404)
+        return result
+
+    @marshal_with(roleplay_info_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('role_name', required=True, help='role_name is required')
+        parser.add_argument('role_desc', required=True, help='role_desc is required')
+        parser.add_argument('depart_id', required=True, help='depart_id is required')
+        parser.add_argument('picture_id', required=True, help='picture_id is required')
+        args = parser.parse_args()
+
+        result = RoleplayInfoHelper.create_case_info(
+            args['role_name'], 
+            args['role_desc'], 
+            args['depart_id'],
+            args['picture_id'],
+            )
+        if not result[0]:
+            abort(404)
+        return RoleplayInfoHelper.get_by_id(result[1])
+
+    @marshal_with(roleplay_info_fields)
+    def put(self, role_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('role_name', dest='name')
+        parser.add_argument('role_desc', dest='description')
+        parser.add_argument('depart_id', dest='depart_id')
+        parser.add_argument('picture_id', dest='picture')
+        args = parser.parse_args()
+
+        fields = filter(lambda x: x[1] is not None, args.iteritems())
+        result = RoleplayInfoHelper.modify(role_id, fields)
+        if not result[0]:
+            abort(404)
+        return RoleplayInfoHelper.get_by_id(role_id)
+
+    @marshal_with(roleplay_info_fields)
+    def delete(self, role_id):
+
+        result = RoleplayInfoHelper.delete_by_id(role_id)
+        if not result[0]:
+            abort(404)
+        return ''
+
+
+class RoleplayPageInfo(Resource):
+    @marshal_with(page_info_fields)
+    def get(self, role_id):
+        result = RoleplayPageInfoHelper.get_by_roleid(role_id)
+        if result is None:
+            print 'no role play info'
+            abort(404)
+        return result
+
+class RoleplayPage(Resource):
+    @marshal_with(roleplay_page_fields)
+    def get(self, role_id, pagination):
+        result = RoleplayPageHelper.get_by_pagination(role_id, pagination)
+        if result is None:
+            abort(404)
+        return result
+
+    @marshal_with(roleplay_info_fields)
+    def post(self, role_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('roleplay_page', required=True, help='roleplay_page is required')
+        args = parser.parse_args()
+
+        page = json.loads(args['roleplay_page'])
+        result = RoleplayPageInfoHelper.create_page_info(
+            role_id, 
+            page['page_info']['page_title'], 
+            page['page_content']
+            )
+        if not result[0]:
+            abort(404)
+        return RoleplayPageHelper.get_by_id(role_id, result[1])
+
+    @marshal_with(roleplay_page_fields)
+    def put(self, role_id, pagination):
+        parser = reqparse.RequestParser()
+        parser.add_argument('roleplay_page', required=True, help='roleplay_page is required')
+        args = parser.parse_args()
+
+        if not RoleplayPageInfoHelper.get_by_pagination(role_id, pagination):
+            abort(404)
+
+        page = json.loads(args['roleplay_page'])
+        result = RoleplayPageInfoHelper.modify(
+            role_id, 
+            pagination,
+            page['page_info']['page_title'], 
+            page['page_content']
+            )
+        if not result[0]:
+            abort(404)
+        return RoleplayPageHelper.get_by_pagination(role_id, pagination)
+
+    def delete(self, role_id, pagination):
+        # TODO: add delete logical
+        result = RoleplayInfoHelper.delete_by_id(role_id)
+        if not result[0]:
+            abort(404)
+        return ''
